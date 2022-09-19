323eb12123c993da8b8c3b00bbd7fc8a552cdc31
Alexander Diakov <alex.dkv@yandex.ru>
2022-09-20 02:02:22 +0300

    added folders and code

---
+++ work1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите рамер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arrayNumbers = new int[size];
+
+void FillInputNumbers(int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введи число {i + 1} ");
+        arrayNumbers[i] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(arrayNumbers[i]);
+    }
+}
+
+int SumOfPositiveNumbers(int[] arrayNumbers)
+{
+    int count = 0;
+    for (int i = 0; i < arrayNumbers.Length; i++)
+    {
+        if (arrayNumbers[i] > 0)
+            count += 1;
+    }  
+    return count;
+}
+
+FillInputNumbers(size);
+Console.WriteLine($"сумма чисел больше 0 = {SumOfPositiveNumbers(arrayNumbers)} ");
